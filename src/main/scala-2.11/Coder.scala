1b1f68e03708c848328f2e3488bf605ff05e0fe2
Alice <alicegutteridge@gmail.com>
2015-04-20 21:00:55 +0100

    processText and tests written. Scaladoc in progress

--- src/main/scala-2.11/Coder.scala
+++ src/main/scala-2.11/Coder.scala
@@ -1,5 +1,7 @@
 package playfair
 
+import scala.annotation.tailrec
+
 class Coder(var keyword: String) {
   keyword = format(keyword)
   val ALPHABET = "ABCDEFGHIKLMNOPQRSTUVWXYZ" // no J
@@ -12,39 +14,69 @@ class Coder(var keyword: String) {
 
   def decode(secretText: String): String = ???
 
-  def generateKeyTable: Unit = {
+
+  /** Creates a 5x5 key table from the keyword.
+    *
+    * Throws an IllegalArgumentException if the array does not have 25 elements.
+    *
+    * @return a 5x5 key table with the keyword and the rest of the alphabet
+    *         (excluding 'J') as unique Chars
+    */
+  def generateKeyTable = {
     val uc = uniqueChars(keyword + ALPHABET)
-    val keyTable = unflatten(uc)
+
+    val keyTable = Array(uc.slice(0, 5),
+                         uc.slice(5, 10),
+                         uc.slice(10, 15),
+                         uc.slice(15, 20),
+                         uc.slice(20, 25))
+    keyTable
   }
 
+  /** Formats a String ready to encode or use in the key table
+   *
+   * @param input text, String
+   * @return a String with only upper case alphabet Chars, and all 'J's replaced with 'I's
+   */
   def format(input: String): String = {
     input.toUpperCase.replaceAll("[^a-zA-Z]", "").replaceAll("J", "I")
   }
 
+  /** Creates an array of unique characters from String
+    *
+    * @param word text, String
+    * @return array of unique characters
+    */
   def uniqueChars(word: String): Array[Char] =
     word.toArray.foldLeft(Array[Char]())((a, b) => if (a.contains(b)) a else a :+ b)
 
-  def unflatten(arr: Array[Char]): Array[Array[Char]] = {
-    Array(arr.slice(0, 5),
-          arr.slice(5, 10),
-          arr.slice(10, 15),
-          arr.slice(15, 20),
-          arr.slice(20, 25))
-  }
+  /** Formats input text according to the Playfair rules.
+    *
+    * ~ All pairs of letters will be separated by spaces,
+    * ~ If a double letter occurs in a pair, an 'X' is inserted between them,
+    * ~ If a pair is "XX", a 'Q' is inserted between them.
+    * ~ If there are an odd number of letters the last will be paired with 'Z',
+    *
+    * @param text String of only A-Z and upper case Chars
+    * @return formatted String
+    */
+  def processText(text:String): String = {
 
-//  def processText(text: String): String = {
-//    def processHelper(acc: Array[Char], text: Array[Char]) = {
-//
-//      if (text.length == 0) acc
-//      else if (text.length == 1) text.head +: ('z' +: acc)
-//      else {
-//        if (text.head == 'x' && text.tail.head == 'x') processHelper()
-//      }
-//      (first == 'x' && second == 'x')
-//    }
-//
-//    processHelper(, text.toArray).toString
-//  }
+    @tailrec
+    def processHelper(acc: Array[Char], text: String): Array[Char] = {
+      if (text.length == 0) acc
+      else if (text.length == 1) 'Z' +: (text.head +: acc)
+      else {
+        if (text.head == 'X' && text.tail.head == 'X')
+          processHelper(' ' +: ('Q' +: ('X' +: acc)), text.tail)
+        else if (text.head == text.tail.head)
+          processHelper(' ' +: ('X' +: (text.head +: acc)), text.tail)
+        else processHelper(' ' +: (text.tail.head +: (text.head +: acc)), text.tail.tail)
+      }
+    }
+
+    new String(processHelper(Array[Char](), text).reverse)
+  }
 
 }
 
--- src/test/scala-2.11/CoderTest.scala
+++ src/test/scala-2.11/CoderTest.scala
@@ -16,13 +16,24 @@ class CoderTest extends FlatSpec with Matchers {
     c.keyword should be("ADSIGIIDFLGASDHGD")
   }
 
-  "An unflatten method " should "return a 5x5 2D array when given an array of 25 elements." in {
-    coder.unflatten(Array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-      'r', 's', 't', 'u', 'v', 'w', 'x', 'y')) should be (Array(Array('a', 'b', 'c', 'd', 'e'),
-                                                                Array('f', 'g', 'h', 'i', 'j'),
-                                                                Array('k', 'l', 'm', 'n', 'o'),
-                                                                Array('p', 'q', 'r', 's', 't'),
-                                                                Array('u', 'v', 'w', 'x', 'y')))
+  "An generateKeyTable method " should "return a 5x5 2D array when given an array of 25 elements." in {
+    new Coder("abcdefghijklmnopqrstuvwxyz").generateKeyTable should be (Array(Array('A', 'B', 'C', 'D', 'E'),
+                                                                              Array('F', 'G', 'H', 'I', 'K'),
+                                                                              Array('L', 'M', 'N', 'O', 'P'),
+                                                                              Array('Q', 'R', 'S', 'T', 'U'),
+                                                                              Array('V', 'W', 'X', 'Y', 'Z')))
+  }
+
+  "A processText method " should "return a String with space-separated pairs." in {
+    coder.processText("ABCDE") should be ("AB CD EZ")
+  }
+
+  it should " insert a Q if there are two Xs in a pair." in {
+    coder.processText("XX") should be ("XQ XZ")
+  }
+
+  it should "insert an X if there are two identical letters in a row." in {
+    coder.processText("AAA") should be ("AX AX AZ")
   }
 
 }
